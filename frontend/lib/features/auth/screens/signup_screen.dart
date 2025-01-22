1a9c07551088b328e746093c69cfcb9e0ae5fd9e
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-22 23:33:18 +0100

    Fix overflowing issue and add TextInputAction to the TextFormFields

--- frontend/lib/features/auth/screens/signup_screen.dart
+++ frontend/lib/features/auth/screens/signup_screen.dart
@@ -16,89 +16,98 @@ class _SignUpScreenState extends State<SignUpScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                "Sign Up.",
-                style: TextStyle(
-                  fontSize: 48,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 32),
-              TextFormField(
-                controller: _nameController,
-                cursorColor: Colors.black,
-                decoration: InputDecoration(hintText: "Name"),
-                validator: (value) {
-                  if (value == null || value.trim().isEmpty) {
-                    return "Name field cannot be empty";
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 16),
-              TextFormField(
-                controller: _emailController,
-                decoration: InputDecoration(hintText: "Email"),
-                validator: (value) {
-                  final regex = RegExp(
-                      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
-
-                  if (value == null) {
-                    return "Email field cannot be empty";
-                  }
-
-                  if (!regex.hasMatch(value.trim())) {
-                    return "Email field contains an invalid email address";
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 16),
-              TextFormField(
-                controller: _passwordController,
-                decoration: InputDecoration(hintText: "Password"),
-                validator: (value) {
-                  if (value == null || value.trim().isEmpty) {
-                    return "Password field cannot be empty";
-                  }
-
-                  if (value.trim().length < 6) {
-                    return "Password field should be at least 7 characters long";
-                  }
-
-                  return null;
-                },
-              ),
-              SizedBox(height: 32),
-              ElevatedButton(
-                onPressed: signUpUser,
-                child: Text(
-                  "SIGN UP",
-                  style: TextStyle(fontSize: 16),
-                ),
-              ),
-              SizedBox(height: 16),
-              RichText(
-                text: TextSpan(
-                  text: "Already have an account? ",
-                  style: TextTheme.of(context).titleMedium,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Center(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    TextSpan(
-                      text: "Sign In",
-                      style: TextStyle(fontWeight: FontWeight.bold),
+                    Text(
+                      "Sign Up.",
+                      style: TextStyle(
+                        fontSize: 48,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
+                    SizedBox(height: 32),
+                    TextFormField(
+                      controller: _nameController,
+                      cursorColor: Colors.black,
+                      decoration: InputDecoration(hintText: "Name"),
+                      textInputAction: TextInputAction.next,
+                      validator: (value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return "Name field cannot be empty";
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 16),
+                    TextFormField(
+                      controller: _emailController,
+                      decoration: InputDecoration(hintText: "Email"),
+                      textInputAction: TextInputAction.next,
+                      validator: (value) {
+                        final regex = RegExp(
+                            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+
+                        if (value == null) {
+                          return "Email field cannot be empty";
+                        }
+
+                        if (!regex.hasMatch(value.trim())) {
+                          return "Email field contains an invalid email address";
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 16),
+                    TextFormField(
+                      controller: _passwordController,
+                      decoration: InputDecoration(hintText: "Password"),
+                      textInputAction: TextInputAction.done,
+                      validator: (value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return "Password field cannot be empty";
+                        }
+
+                        if (value.trim().length < 6) {
+                          return "Password field should be at least 7 characters long";
+                        }
+
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 32),
+                    ElevatedButton(
+                      onPressed: signUpUser,
+                      child: Text(
+                        "SIGN UP",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    RichText(
+                      text: TextSpan(
+                        text: "Already have an account? ",
+                        style: TextTheme.of(context).titleMedium,
+                        children: [
+                          TextSpan(
+                            text: "Sign In",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    )
                   ],
                 ),
-              )
-            ],
+              ),
+            ),
           ),
         ),
       ),
