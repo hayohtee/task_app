b651ace93a62ab3bf2d68aa72a237fe9d1c7f590
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-22 23:45:32 +0100

    Add LoginScreen

---
+++ frontend/lib/features/auth/screens/login_screen.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Center(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Login.",
+                      style: TextStyle(
+                        fontSize: 48,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 32),
+                    TextFormField(
+                      controller: _emailController,
+                      decoration: InputDecoration(hintText: "Email"),
+                      textInputAction: TextInputAction.next,
+                      keyboardType: TextInputType.emailAddress,
+                      validator: (value) {
+                        final regex = RegExp(
+                            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+
+                        if (value == null) {
+                          return "Email field cannot be empty";
+                        }
+
+                        if (!regex.hasMatch(value.trim())) {
+                          return "Email field contains an invalid email address";
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 16),
+                    TextFormField(
+                      controller: _passwordController,
+                      decoration: InputDecoration(hintText: "Password"),
+                      textInputAction: TextInputAction.done,
+                      keyboardType: TextInputType.visiblePassword,
+                      validator: (value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return "Password field cannot be empty";
+                        }
+
+                        if (value.trim().length < 7) {
+                          return "Password field should be at least 7 characters long";
+                        }
+
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 32),
+                    ElevatedButton(
+                      onPressed: loginUser,
+                      child: Text(
+                        "LOGIN",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    RichText(
+                      text: TextSpan(
+                        text: "Don't have an account? ",
+                        style: TextTheme.of(context).titleMedium,
+                        children: [
+                          TextSpan(
+                            text: "Sign Up",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void loginUser() {
+    if (_formKey.currentState!.validate()) {}
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+}
