c641da0d2e87707632a357dbcc10d7c4d7c2d27e
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-22 22:46:39 +0100

    Added TextEditingControllers and dispose method

--- frontend/lib/features/auth/screens/signup_screen.dart
+++ frontend/lib/features/auth/screens/signup_screen.dart
@@ -8,12 +8,38 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _nameController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Signup Screen"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Sign Up.",
+              style: TextStyle(
+                fontSize: 48,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            TextFormField()
+          ],
+        ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    _nameController.dispose();
+    super.dispose();
   }
 }
