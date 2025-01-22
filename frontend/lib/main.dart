11d9fb8b120379fcc9f9fae139b5867c63847e28
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-22 22:40:03 +0100

    Add SignUpScreen as the home widget

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/features/auth/screens/signup_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,6 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: SignUpScreen(),
     );
   }
 }
