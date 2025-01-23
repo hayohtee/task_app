3cf35fb7c3d6001b95ced13b3ae8605b271077cb
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-23 12:26:09 +0100

    Add background method

--- backend/cmd/api/helpers.go
+++ backend/cmd/api/helpers.go
@@ -101,3 +101,29 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any
 
 	return nil
 }
+
+// background is a helper method for running background
+// tasks.
+// It accepts arbitrary function as parameter and run it
+// in a goroutine, it also recovers any panic when calling
+// the function.
+func (app *application) background(fn func()) {
+	// Increment the WaitGroup counter.
+	app.wg.Add(1)
+
+	// Launch background goroutine.
+	go func() {
+		// Use defer to decrement the WaitGroup counter before goroutine returns.
+		defer app.wg.Done()
+
+		// Recover any panic.
+		defer func() {
+			if err := recover(); err != nil {
+				app.logger.Error(fmt.Sprintf("%v", err))
+			}
+		}()
+
+		// Execute the function.
+		fn()
+	}()
+}
