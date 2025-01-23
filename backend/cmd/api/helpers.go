218ce475c8f71b6221565ce654b0ca6e21ecbbf3
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-23 12:21:30 +0100

    Add readIDParam method

--- backend/cmd/api/helpers.go
+++ backend/cmd/api/helpers.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"errors"
+	"net/http"
+	"strconv"
+)
+
 // envelope is a custom type for enveloping JSON response.
 type envelope map[string]any
 
+func (app *application) readIDParam(r *http.Request) (int64, error) {
+	id, err := strconv.ParseInt(r.PathValue("id"), 10, 64)
+	if err != nil {
+		return 0, errors.New("invalid id parameter")
+	}
+	return id, nil
+}
