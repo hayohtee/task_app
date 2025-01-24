0b78458b55c394676e28b94b6261815c1daf146a
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-24 14:39:30 +0100

    Add ValidateEmail function

--- backend/internal/data/user.go
+++ backend/internal/data/user.go
@@ -3,6 +3,7 @@ package data
 import (
 	"time"
 
+	"github.com/hayohtee/task_app/internal/validator"
 	uuid "github.com/jackc/pgx/pgtype/ext/gofrs-uuid"
 )
 
@@ -14,4 +15,9 @@ type User struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 	Version   int       `json:"-"`
+}
+
+func ValidateEmail(v *validator.Validator, email string) {
+	v.Check(email != "", "email", "must be provided")
+	v.Check(validator.Matches(email, validator.EmailRX), "email", "must be a valid email address")
 }
