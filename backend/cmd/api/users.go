15d31b617f664da7535f95d9e4a91d5337cdf18d
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-24 15:20:46 +0100

    Write the code for registerUserHandler

--- backend/cmd/api/users.go
+++ backend/cmd/api/users.go
@@ -1,7 +1,53 @@
 package main
 
-import "net/http"
+import (
+	"errors"
+	"net/http"
+
+	"github.com/hayohtee/task_app/internal/data"
+	"github.com/hayohtee/task_app/internal/validator"
+)
 
 func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Request) {
-	
+	var input struct {
+		Name     string `json:"name"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	if err := app.readJSON(w, r, &input); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	user := data.User{
+		Name:  input.Name,
+		Email: input.Email,
+	}
+
+	if err := user.Password.Set(input.Password); err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	v := validator.New()
+	if data.ValidateUser(v, user); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	if err := app.model.Users.Insert(&user); err != nil {
+		switch {
+		case errors.Is(err, data.ErrDuplicateEmail):
+			v.AddError("email", "a user with this email already exist")
+			app.failedValidationResponse(w, r, v.Errors)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	if err := app.writeJSON(w, http.StatusCreated, envelope{"user": user}, nil); err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
