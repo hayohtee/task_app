fac77358cdc5167ea39829b6a517be32809f56ef
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-24 14:25:51 +0100

    Add Set method on the password struct

--- backend/internal/data/user.go
+++ backend/internal/data/user.go
@@ -1,5 +1,7 @@
 package data
 
+import "golang.org/x/crypto/bcrypt"
+
 // password is a struct containing the plaintext and hashed
 // versions of the password for a user.
 //
@@ -9,4 +11,18 @@ package data
 type password struct {
 	plaintext *string
 	hash      []byte
+}
+
+// Set calculates the bycrypt hash of plaintextPassword and stores
+// both the hash and the plaintext versions in the struct.
+func (p *password) Set(plaintextPassword string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(plaintextPassword), 12)
+	if err != nil {
+		return err
+	}
+
+	p.plaintext = &plaintextPassword
+	p.hash = hash
+
+	return nil
 }
