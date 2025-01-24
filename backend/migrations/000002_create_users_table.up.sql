785d2f79749343b9a9c925f006e9b139b5e6e414
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-24 14:47:21 +0100

    Change password to password_hash

--- backend/migrations/000002_create_users_table.up.sql
+++ backend/migrations/000002_create_users_table.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS users(
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name TEXT NOT NULL,
     email citext UNIQUE NOT NULL,
-    password BYTEA NOT NULL,
+    password_hash BYTEA NOT NULL,
     created_at TIMESTAMP(0) WITH TIME ZONE DEFAULT now(),
     updated_at TIMESTAMP(0) WITH TIME ZONE DEFAULT now(),
     version INTEGER NOT NULL DEFAULT 1
