3c9205b97eb5ab5fa562be3acddd93fecc51a467
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-24 14:45:41 +0100

    Fix syntax issue

--- backend/migrations/000002_create_users_table.up.sql
+++ backend/migrations/000002_create_users_table.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS users(
     name TEXT NOT NULL,
     email citext UNIQUE NOT NULL,
     password BYTEA NOT NULL,
-    created_at TIMESTAMP(0) WITH TIME ZONE now()
-    updated_at TIMESTAMP(0) WITH TIME ZONE now()
+    created_at TIMESTAMP(0) WITH TIME ZONE DEFAULT now(),
+    updated_at TIMESTAMP(0) WITH TIME ZONE DEFAULT now(),
     version INTEGER NOT NULL DEFAULT 1
 );
