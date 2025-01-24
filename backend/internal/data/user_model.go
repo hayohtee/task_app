fb87d5aea10a9823cff8e4e22f24672ea1878635
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-24 14:48:39 +0100

    Add ErrDuplicateEmail variable

--- backend/internal/data/user_model.go
+++ backend/internal/data/user_model.go
@@ -1,6 +1,13 @@
 package data
 
-import "database/sql"
+import (
+	"database/sql"
+	"errors"
+)
+
+var (
+	ErrDuplicateEmail = errors.New("duplicate email")
+)
 
 type UserModel struct {
 	DB *sql.DB
