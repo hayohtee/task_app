a1c0e5cba303288f2ff81f7ec80251a91ae4040b
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-25 13:50:03 +0100

    Wrap Material app with MultiBlocProvider

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:frontend/features/auth/cubit/auth_cubit.dart';
 import 'package:frontend/features/auth/screens/login_screen.dart';
 
 void main() {
@@ -8,54 +10,58 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Task App',
-      theme: ThemeData(
-        inputDecorationTheme: InputDecorationTheme(
-          contentPadding: EdgeInsets.all(24),
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(
-              color: Colors.grey.shade300,
-              width: 2,
-            ),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(
-              width: 2,
-            ),
-          ),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(
-              width: 2,
-            ),
-          ),
-          errorBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(
-              width: 2,
-              color: Colors.red,
-            ),
-          ),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.black,
-            foregroundColor: Colors.white,
-            minimumSize: Size(double.maxFinite, 60),
-            shape: RoundedRectangleBorder(
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => AuthCubit())
+      ],
+      child: MaterialApp(
+        title: 'Task App',
+        theme: ThemeData(
+          inputDecorationTheme: InputDecorationTheme(
+            contentPadding: EdgeInsets.all(24),
+            enabledBorder: OutlineInputBorder(
               borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(
+                color: Colors.grey.shade300,
+                width: 2,
+              ),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(
+                width: 2,
+              ),
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(
+                width: 2,
+              ),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(
+                width: 2,
+                color: Colors.red,
+              ),
             ),
           ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.black,
+              foregroundColor: Colors.white,
+              minimumSize: Size(double.maxFinite, 60),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+          useMaterial3: true,
         ),
-        useMaterial3: true,
+        home: LoginScreen(),
       ),
-      home: LoginScreen(),
     );
   }
 }
