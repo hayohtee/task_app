b75ea8def47f371089ea4a75c73bcb4a9df1b37a
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-25 13:49:11 +0100

    Update UserModel

--- frontend/lib/models/user_model.dart
+++ frontend/lib/models/user_model.dart
@@ -15,7 +15,6 @@ class UserModel {
   final String name;
   final DateTime createdAt;
   final DateTime updatedAt;
-  
 
   UserModel copyWith({
     String? id,
@@ -48,8 +47,8 @@ class UserModel {
       id: map['id'] as String,
       email: map['email'] as String,
       name: map['name'] as String,
-      createdAt: DateTime.fromMillisecondsSinceEpoch(map['created_at'] as int),
-      updatedAt: DateTime.fromMillisecondsSinceEpoch(map['updated_at'] as int),
+      createdAt: DateTime.parse(map['created_at']),
+      updatedAt: DateTime.parse(map['updated_at']),
     );
   }
 
@@ -67,21 +66,16 @@ class UserModel {
   @override
   bool operator ==(covariant UserModel other) {
     if (identical(this, other)) return true;
-  
-    return 
-      other.id == id &&
-      other.email == email &&
-      other.name == name &&
-      other.createdAt == createdAt &&
-      other.updatedAt == updatedAt;
+
+    return other.id == id &&
+        other.email == email &&
+        other.name == name &&
+        other.createdAt == createdAt &&
+        other.updatedAt == updatedAt;
   }
 
   @override
   int get hashCode {
-    return id.hashCode ^
-      email.hashCode ^
-      name.hashCode ^
-      createdAt.hashCode ^
-      updatedAt.hashCode;
+    return id.hashCode ^ email.hashCode ^ name.hashCode ^ createdAt.hashCode ^ updatedAt.hashCode;
   }
 }
