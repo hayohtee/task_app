a30efc7a6ab5cc9fda39e7ca5783d29b977a9204
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-25 11:37:21 +0100

    Add requireAuthentication middleware

--- backend/cmd/api/middlewares.go
+++ backend/cmd/api/middlewares.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
+	"strings"
+
+	"github.com/hayohtee/task_app/internal/data"
+	"github.com/hayohtee/task_app/internal/validator"
 )
 
 // recoverPanic is a middleware that recovers from any panics and writes a 500 Internal Server Error
@@ -30,6 +35,75 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 			}
 		}()
 
+		next.ServeHTTP(w, r)
+	})
+}
+
+// requireAuthentication is a middleware function that ensures the incoming request
+// contains a valid authentication token in the Authorization header. If the token
+// is missing, invalid, or expired, it sends a 401 Unauthorized response. Otherwise,
+// it retrieves the token from Redis, validates its scope, and sets the user ID in
+// the request context before passing control to the next handler in the chain.
+//
+// The token must be in the format "Bearer <token>". The middleware checks the token
+// format, validates it, and retrieves the corresponding token from Redis. If the
+// token is valid and has the correct scope, the user ID is set in the request context.
+//
+// Parameters:
+// - next: The next http.HandlerFunc in the middleware chain.
+//
+// Returns:
+// - An http.HandlerFunc that wraps the next handler with authentication checks.
+func (app *application) requireAuthentication(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Get the value of the Authorization header from the request. If there isn't any
+		// value present, call the app.invalidAuthenticationTokenResponse() helper to send
+		// a 401 Unauthorized response, and return from the middleware chain.
+		w.Header().Add("Vary", "Authorization")
+		authorizationHeader := r.Header.Get("Authorization")
+		if authorizationHeader == "" {
+			app.invalidAuthenticationTokenResponse(w, r)
+			return
+		}
+
+		// Otherwise, extract the authentication token from the header.
+		headerParts := strings.Split(authorizationHeader, " ")
+		if len(headerParts) != 2 || headerParts[0] != "Bearer" {
+			app.invalidAuthenticationTokenResponse(w, r)
+			return
+		}
+
+		// The value of the token will be in the second position in the headerParts slice.
+		// We extract this and assign it to the token variable.
+		tokenPlainText := headerParts[1]
+
+		// Validate the token to make sure it is in a sensible format. If it's not, then
+		// call the app.invalidAuthenticationTokenResponse() helper to send a 401 Unauthorized
+		// response, and return from the middleware chain.
+		v := validator.New()
+		if data.ValidateTokenPlainText(v, "access_token", tokenPlainText); !v.Valid() {
+			app.invalidAuthenticationTokenResponse(w, r)
+			return
+		}
+
+		// Attempt to retrieve a token from Redis using the plain text token as the key
+		token, err := app.model.Tokens.Get(tokenPlainText)
+		if err != nil {
+			switch {
+			case errors.Is(err, data.ErrRecordNotFound):
+				app.invalidAuthenticationTokenResponse(w, r)
+			default:
+				app.serverErrorResponse(w, r, err)
+			}
+			return
+		}
+
+		if token.Scope != data.ScopeAccess {
+			app.errorResponse(w, r, http.StatusUnauthorized, "require an access token")
+			return
+		}
+
+		r = app.contextSetUserID(r, userContextKey, token)
 		next.ServeHTTP(w, r)
 	})
 }
