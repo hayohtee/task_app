52f852a6a9afd83a6e61e16e2941af6b0d74568a
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-26 12:33:33 +0100

    Add FailedValidationError

--- frontend/lib/features/auth/repository/remote_response.dart
+++ frontend/lib/features/auth/repository/remote_response.dart
@@ -1,1 +1,13 @@
 sealed class RemoteResponse {}
+
+class FailedValidationError extends RemoteResponse {
+  FailedValidationError({
+    required this.email,
+    required this.name,
+    required this.password,
+  });
+
+  final String name;
+  final String email;
+  final String password;
+}
