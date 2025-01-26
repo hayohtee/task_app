9a15e3ac0877aee46812810f0866a7ef21a8ffa2
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-26 12:38:09 +0100

    Generate json serialization on FailedValidationError

--- frontend/lib/features/auth/repository/remote_response.dart
+++ frontend/lib/features/auth/repository/remote_response.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 sealed class RemoteResponse {}
 
 class FailedValidationError extends RemoteResponse {
@@ -10,4 +13,27 @@ class FailedValidationError extends RemoteResponse {
   final String name;
   final String email;
   final String password;
+
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'name': name,
+      'email': email,
+      'password': password,
+    };
+  }
+
+  factory FailedValidationError.fromMap(Map<String, dynamic> map) {
+    return FailedValidationError(
+      name: map['name'] as String,
+      email: map['email'] as String,
+      password: map['password'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory FailedValidationError.fromJson(String source) {
+    return FailedValidationError.fromMap(json.decode(source) as Map<String, dynamic>);
+  }
 }
