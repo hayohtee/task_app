7b1c24f2b1300fb33f328dfbe93ef2668ace582b
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-26 12:47:19 +0100

    Add Error class

--- frontend/lib/features/auth/repository/remote_response.dart
+++ frontend/lib/features/auth/repository/remote_response.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 part of "remote_repository.dart";
 
 sealed class RemoteResponse {}
@@ -60,5 +63,29 @@ class UserCreated extends RemoteResponse {
 
   factory UserCreated.fromJson(String source) {
     return UserCreated.fromMap(json.decode(source) as Map<String, dynamic>);
+  }
+}
+
+class Error extends RemoteResponse {
+  Error({ required this.error});
+
+  final String error;
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'error': error,
+    };
+  }
+
+  factory Error.fromMap(Map<String, dynamic> map) {
+    return Error(
+      error: map['error'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Error.fromJson(String source) {
+    return Error.fromMap(json.decode(source) as Map<String, dynamic>);
   }
 }
