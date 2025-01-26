d7042f8413457ea360a096345b1a528ca521a3bd
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-26 12:41:35 +0100

    Add CreatedResponse

--- frontend/lib/features/auth/repository/remote_response.dart
+++ frontend/lib/features/auth/repository/remote_response.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 
+import 'package:frontend/models/token_model.dart';
+import 'package:frontend/models/user_model.dart';
+
 sealed class RemoteResponse {}
 
 class FailedValidationError extends RemoteResponse {
@@ -13,7 +16,6 @@ class FailedValidationError extends RemoteResponse {
   final String name;
   final String email;
   final String password;
-
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
@@ -35,5 +37,32 @@ class FailedValidationError extends RemoteResponse {
 
   factory FailedValidationError.fromJson(String source) {
     return FailedValidationError.fromMap(json.decode(source) as Map<String, dynamic>);
+  }
+}
+
+class CreatedResponse extends RemoteResponse {
+  CreatedResponse({required this.tokens, required this.user});
+
+  final TokenModel tokens;
+  final UserModel user;
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'tokens': tokens.toMap(),
+      'user': user.toMap(),
+    };
+  }
+
+  factory CreatedResponse.fromMap(Map<String, dynamic> map) {
+    return CreatedResponse(
+      tokens: TokenModel.fromMap(map['tokens'] as Map<String, dynamic>),
+      user: UserModel.fromMap(map['user'] as Map<String, dynamic>),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory CreatedResponse.fromJson(String source) {
+    return CreatedResponse.fromMap(json.decode(source) as Map<String, dynamic>);
   }
 }
