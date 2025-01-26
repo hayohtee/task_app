88f5dce959baf2e9b077a46713b60467d35fdd08
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-26 12:55:45 +0100

    Add SignUpValidationMessage class

--- frontend/lib/features/auth/repository/remote_response.dart
+++ frontend/lib/features/auth/repository/remote_response.dart
@@ -1,6 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'dart:convert';
-
 part of "remote_repository.dart";
 
 sealed class RemoteResponse {}
@@ -67,7 +64,7 @@ class UserCreated extends RemoteResponse {
 }
 
 class Error extends RemoteResponse {
-  Error({ required this.error});
+  Error({required this.error});
 
   final String error;
 
@@ -87,5 +84,39 @@ class Error extends RemoteResponse {
 
   factory Error.fromJson(String source) {
     return Error.fromMap(json.decode(source) as Map<String, dynamic>);
+  }
+}
+
+class SignUpValidationMessage {
+  const SignUpValidationMessage({
+    required this.name,
+    required this.email,
+    required this.password,
+  });
+
+  final String name;
+  final String email;
+  final String password;
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'name': name,
+      'email': email,
+      'password': password,
+    };
+  }
+
+  factory SignUpValidationMessage.fromMap(Map<String, dynamic> map) {
+    return SignUpValidationMessage(
+      name: map['name'] as String,
+      email: map['email'] as String,
+      password: map['password'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory SignUpValidationMessage.fromJson(String source) {
+    return SignUpValidationMessage.fromMap(json.decode(source) as Map<String, dynamic>);
   }
 }
