6f80ef1759e7079867a6f23c4833074f49ab4b4e
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-30 15:49:39 +0100

    Add login_cubit.dart

---
+++ frontend/lib/features/auth/screens/login/cubit/login_cubit.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:frontend/features/auth/repository/remote_repository.dart';
+import 'package:frontend/models/token_model.dart';
+import 'package:frontend/models/user_model.dart';
+
+part 'login_state.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  LoginCubit() : super(LoginInitial());
+
+  final repository = RemoteRepository();
+
+  void login(String email, String password) async {
+    emit(LoginLoading());
+
+    final response = await repository.login(email: email, password: password);
+    switch (response) {
+      case Success():
+        emit(LoginSuccess(tokens: response.tokens, user: response.user));
+        break;
+      case LoginFailedValidationError():
+        emit(LoginValidationError(
+          email: response.email,
+          password: response.password,
+        ));
+        break;
+      case EmailNotFound():
+        emit(LoginEmailNotFoundError(response.message));
+        break;
+      case InvalidCredentials():
+        emit(LoginInvalidCredentialsError(response.message));
+        break;
+      case Error():
+        emit(LoginError(response.error));
+        break;
+    }
+  }
+}
